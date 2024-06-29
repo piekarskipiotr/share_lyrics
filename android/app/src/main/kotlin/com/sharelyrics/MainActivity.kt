4837d955ab1690222531455382a13c0819ee6e33
piekarskipiotr <ppiekarski8@gmail.com>
2024-06-29 20:18:30 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/sharelyrics/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sharelyrics.share_lyrics
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:share_lyrics/app/app.dart';
+import 'package:share_lyrics/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
