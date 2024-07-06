5a9829e3401ba6b0e71f90846267a23e3e65e7b1
piekarskipiotr <ppiekarski8@gmail.com>
2024-07-06 08:26:59 +0200

    Add no lyrics empty state

--- lib/presentation/song_details/widgets/song_lyrics/song_lyrics_view.dart
+++ lib/presentation/song_details/widgets/song_lyrics/song_lyrics_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:share_lyrics/data/models/models.dart';
+import 'package:share_lyrics/design_system/design_system.dart';
+import 'package:share_lyrics/l10n/l10n.dart';
 import 'package:share_lyrics/presentation/song_details/widgets/song_lyrics/song_lyrics_line.dart';
 
 class SongLyricsView extends StatelessWidget {
@@ -10,11 +12,26 @@ class SongLyricsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    if (lyrics.isEmpty) return _buildEmptyView(context);
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 4),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: lyrics.map((lyric) => SongLyricsLine(lyric: lyric, onTap: onTap)).toList(),
+      ),
+    );
+  }
+
+  Widget _buildEmptyView(BuildContext context) {
+    final l10n = context.l10n;
+    return Padding(
+      padding: const EdgeInsets.all(48),
+      child: Center(
+        child: Text(
+          l10n.empty_lyrics,
+          textAlign: TextAlign.center,
+          style: AppTextStyles.h9(color: AppColors.white.withOpacity(0.75)),
+        ),
       ),
     );
   }
