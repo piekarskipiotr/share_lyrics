f2300d124ec64b895c259a18580c8f2052dbaa82
piekarskipiotr <ppiekarski8@gmail.com>
2024-07-10 22:15:21 +0200

    Fix permission function

--- lib/utils/helpers/permission_helper.dart
+++ lib/utils/helpers/permission_helper.dart
@@ -3,7 +3,7 @@ import 'package:permission_handler/permission_handler.dart';
 
 class PermissionHelper {
   static Future<void> check(Permission permission, VoidCallback onSuccess) async {
-    final status = await Permission.photos.status;
+    final status = await permission.status;
     switch (status) {
       case PermissionStatus.granted: onSuccess.call();
       case PermissionStatus.permanentlyDenied: await openAppSettings();
