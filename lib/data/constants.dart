abc657d9b73e6c1c1150075499f968eb18dc0e58
piekarskipiotr <ppiekarski8@gmail.com>
2024-07-16 10:19:40 +0200

    Update terms of service and privacy policy url

--- lib/data/constants.dart
+++ lib/data/constants.dart
@@ -1,3 +1,3 @@
-const termsOfServiceUrl = 'https://github.com/piekarskipiotr';
-const privacyPolicyUrl = 'https://github.com/piekarskipiotr';
+const termsOfServiceUrl = 'https://piekarskipiotr.com/share_lyrics_terms_n_conditions.html';
+const privacyPolicyUrl = 'https://piekarskipiotr.com/share_lyrics_privacy_policy.html';
 const geniusAPIUrl = 'https://api.genius.com/';
